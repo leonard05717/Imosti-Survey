a650a3d8e4f712f50cd2df7e2ef0c0312902e86a
Ronald Gulayan <ronaldgulayan00@gmail.com>
2025-07-23 18:36:02 +0800

    up

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,135 +1,133 @@
 import React, { useEffect, useState } from "react";
-import './LoginPage.css';
-import { AspectRatio ,Alert, Image, ActionIcon, PasswordInput } from '@mantine/core';
+import "./LoginPage.css";
+import {
+  AspectRatio,
+  Alert,
+  Image,
+  ActionIcon,
+  PasswordInput,
+} from "@mantine/core";
 import supabase from "./supabase";
-import { data, useNavigate } from 'react-router-dom';
-import { IconEye, IconInfoCircle } from '@tabler/icons-react';
-
+import { data, useNavigate } from "react-router-dom";
+import { IconEye, IconInfoCircle } from "@tabler/icons-react";
 
 function LoginPage() {
-  const navigate = useNavigate()
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const [Account , setaccount] = useState([])
-  
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [Account, setaccount] = useState([]);
 
-  
   const icon = <IconInfoCircle />;
 
   async function submit() {
-    
-   const data = Account.find((v) => v.Email === email && v.Role === password && v.Status === "Active");
-    if(data){
-      window.localStorage.setItem("data" ,JSON.stringify(data))
-      navigate("/admin")
-      console.log("sample1")
+    const data = Account.find(
+      (v) => v.Email === email && v.Role === password && v.Status === "Active",
+    );
+    if (data) {
+      window.localStorage.setItem("data", JSON.stringify(data));
+      navigate("/admin");
+      console.log("sample1");
+    } else {
+      console.log("Account not found");
     }
-    else{
-      console.log('Account not found')
-    }
-   
   }
 
   async function loadData() {
     const { error, data } = await supabase.from("Staff-Info").select("*");
-    setaccount(data) 
+    setaccount(data);
   }
 
   useEffect(() => {
-    loadData()
-    
+    loadData();
+
     const sectionSubscription = supabase
-    .channel("realtime:Staff-Info")
-    .on(
-      "postgres_changes",
-      { event: "*", schema: "public", table: "Staff-Info" },
-      (payload) => {
-        if (payload.eventType === "INSERT") {
-          setaccount((prev) => [
-            payload.new,
-            ...prev,
-          ]);
-        } else if (payload.eventType === "UPDATE") {
-          setaccount((prev) =>
-            prev.map((item) =>
-              item.id === payload.new.id
-                ? (payload.new)
-                : item
-            )
-          );
-        } else if (payload.eventType === "DELETE") {
-          setaccount((prev) =>
-            prev.filter((item) => item.id !== payload.old.id)
-          );
-        }
-      }
-    )
-    .subscribe();
+      .channel("realtime:Staff-Info")
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "Staff-Info" },
+        (payload) => {
+          if (payload.eventType === "INSERT") {
+            setaccount((prev) => [payload.new, ...prev]);
+          } else if (payload.eventType === "UPDATE") {
+            setaccount((prev) =>
+              prev.map((item) =>
+                item.id === payload.new.id ? payload.new : item,
+              ),
+            );
+          } else if (payload.eventType === "DELETE") {
+            setaccount((prev) =>
+              prev.filter((item) => item.id !== payload.old.id),
+            );
+          }
+        },
+      )
+      .subscribe();
 
     return () => {
-      supabase.removeChannel(sectionSubscription)
-    }
-
-    
-  }, [])
+      supabase.removeChannel(sectionSubscription);
+    };
+  }, []);
   return (
     <>
-       <div style={{backgroundColor:'black'}}>
-      
-      <div className='logo' style={{ display: 'flex' }}> 
-                <AspectRatio style={{  marginTop: '10px' , marginBottom: '30px'} } ratio={1} flex="0 0 200px" >
-                
-                <Image 
-
-                h={100} 
-                w={300}
-                   src="../Picture/Admin-Logo.png"
-                  alt="Avatar"
-              />
-                    </AspectRatio>
-                    
-                  
-          </div>
+      <div style={{ backgroundColor: "black" }}>
+        <div
+          className='logo'
+          style={{ display: "flex" }}
+        >
+          <AspectRatio
+            style={{ marginTop: "10px", marginBottom: "30px" }}
+            ratio={1}
+            flex='0 0 200px'
+          >
+            <Image
+              h={100}
+              w={300}
+              src='../Picture/Admin-Logo.png'
+              alt='Avatar'
+            />
+          </AspectRatio>
+        </div>
       </div>
 
-      <div className="container">
-        <div className="login-box">
+      <div className='container'>
+        <div className='login-box'>
           <h2>Log In Account</h2>
           <hr />
-          <form >
+          <form>
             <label>Email Address</label>
             <input
-              type="email"
+              type='email'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
             <div>
-            <label>Password</label>
-            <PasswordInput
-                 size="lg"
-                 placeholder="Input placeholder"
-                 onChange={(e) => setPassword(e.target.value)}
+              <label>Password</label>
+              <PasswordInput
+                size='lg'
+                placeholder='Input placeholder'
+                onChange={(e) => setPassword(e.target.value)}
               />
-             
             </div>
 
-            <div className="forgot-password">
-              <a href="#">Forgot Password?</a>
+            <div className='forgot-password'>
+              <a href='#'>Forgot Password?</a>
             </div>
 
-            <button type="submit" onClick={() => {
-              submit()
-            }}>Sign In</button>
-            
+            <button
+              type='submit'
+              onClick={() => {
+                submit();
+              }}
+            >
+              Sign In
+            </button>
           </form>
         </div>
       </div>
-
-      
     </>
   );
 }
 
-export default LoginPage
+export default LoginPage;
