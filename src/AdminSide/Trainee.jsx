0a095d938bc717a0769cf0f13f265e5583e5b811
Leonard Cruz <leonardcruz0705@gmail.com>
2025-09-02 12:04:07 +0800

    leo

--- src/AdminSide/Trainee.jsx
+++ src/AdminSide/Trainee.jsx
@@ -127,6 +127,7 @@ function Trainee() {
       
         await fetchData();
         closeDeleteRecord();
+        setcomfirmdelete("");
         console.log("Delete Success");
         console.log(selectedDateRange[0], selectedDateRange[1]);
         console.log(checkedStudents);
